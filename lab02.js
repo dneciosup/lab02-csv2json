10e0994d4dfd60f700d787e9bb3d15c52b1bb343
Danilo Neciosup <65529691+dneciosup@users.noreply.github.com>
2020-08-10 14:16:29 -0500

    Update lab02.js

--- lab02.js
+++ lab02.js
@@ -3,12 +3,15 @@ var path = require('path')
 
 var csvjson = require('csvjson')
 
-var read = fs.createReadStream(path.join(__dirname, 'customer-data.xls'))
+var fromFName ='customer-data.xls'
+var toFName = 'customer-data.json'
+
+var read = fs.createReadStream(path.join(__dirname, fromFName))
 console.log('Reading file to convert...')
-var write = fs.createWriteStream(path.join(__dirname, 'customer-data.json'))
+var write = fs.createWriteStream(path.join(__dirname, toFName))
 console.log('Creating file to dump json...')
 var toObject = csvjson.stream.toObject()
-var stringify = csvjson.stream.stringify()
+var stringify = csvjson.stream.stringify(2)
 console.log('Defining parameters to convert')
 read.pipe(toObject).pipe(stringify).pipe(write);
 console.log('Documment done Successful.')
